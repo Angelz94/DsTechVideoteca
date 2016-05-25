a60ba836385b78122bf13d14f110f394935f7b5c
Valerio <Valerio@192.168.7.244>
2016-05-25 10:26:26 +0200

    -

--- DsTechVideoteca/src/main/java/org/dstech/videoteca/configuration/SecurityWebApplicationInitializer.java
+++ DsTechVideoteca/src/main/java/org/dstech/videoteca/configuration/SecurityWebApplicationInitializer.java
@@ -1,7 +1,7 @@
-package org.dstech.videoteca.configuration;
+/*package org.dstech.videoteca.configuration;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
 
-}
+}*/
